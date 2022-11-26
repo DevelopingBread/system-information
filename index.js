371a276ea14e45aabd37b021f158a4f523ab1e00
__metamethods <tabledmetamethods@gmail.com>
2022-11-25 21:58:46 -0800

    Added some more icons

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { setTimeout } = require('timers/promises');
 const discord_rpc = require('discord-rpc');
 const os = require('os');
 const activeWindow = require('active-win')
+const Thread = require('worker_threads')
 
 const client = new discord_rpc.Client({ transport: 'ipc' });
 
@@ -15,7 +16,34 @@ const os_platform = os.platform();
 const os_user = os.userInfo().username;
 
 // Tables
-const activity_data = {
+const icons = {
+  os: {
+    'Mac OS': 'os_mac',
+    'win32': 'os_windows',
+    'linux': 'os_linux'
+  },
+
+  apps: {
+    'Visual Studio Code': 'app_vsc',
+    'RobloxStudio': 'app_rblx_studio',
+
+    'Windows Explorer': 'app_explorer',
+    'Notepad': 'app_notepad',
+    'Settings': 'app_win_settings',
+
+    'Discord': 'app_discord',
+
+    'Google Chrome': 'app_chrome',
+    'Firefox': 'app_firefox'
+  }
+}
+
+const replaceNames = {
+  'SearchHost.exe': 'Settings',
+  'Notepad.exe': 'Notepad'
+}
+
+var activity_data = {
   pid: process.pid,
   activity: {
     details: '[Details]',
@@ -27,7 +55,7 @@ const activity_data = {
       large_image: 'os_windows',
       small_image: 'unknown',
 
-      large_text: `Windows v${os_release}`,
+      large_text: `${ CapitalizeFirstLetter( (icons.os[os_platform] || 'unknown os').replace('os_', '') ) } v${os_release}`,
       small_text: 'unknown'
     },
 
@@ -35,13 +63,8 @@ const activity_data = {
   }
 }
 
-const icons = {
-  os: {
-    'Mac OS': 'os_mac',
-    'win32': 'os_windows',
-    'linux': 'os_linux'
-  }
-}
+// Variables
+var currentWindow = '';
 
 // Functions
 function Print(type, output) {
@@ -49,12 +72,38 @@ function Print(type, output) {
 }
 
 function Update() {
-  Print('Update', 'Updating activity.')
-  client.request('SET_ACTIVITY', activity_data)
+  Print('Update', 'Updating activity.');
+  client.request('SET_ACTIVITY', activity_data);
+  lastActivityData = activity_data;
 }
 
 function CapitalizeFirstLetter(string) { 
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function SetWindowData() {
+  activity_data.activity.state = `Doing: ${currentWindow}`;
+
+  Print('Window Data', 'Set Window data');
+}
+
+function SetWindowIconData() {
+  activity_data.activity.assets.small_image = icons.apps[currentWindow] || 'app_unknown';
+  activity_data.activity.assets.small_text = currentWindow || 'unknown app';
+
+  Print('Icon', 'Set Icon data');
+}
+
+async function GetWindow() {
+  var data = await activeWindow();
+
+  Print('Get Window', 'Got Active Window');
+
+  if (data == undefined) {
+    return 'Nothing';
+  }
+
+  return data.owner.name;
 }
 
 // Main
@@ -68,17 +117,25 @@ client.on('ready', async () => {
   
   activity_data.activity.assets.large_image = icons.os[os_platform] || 'os_unknown';
 
-  activity_data.activity.details = `${ CapitalizeFirstLetter( (icons.os[os_platform] || 'os_unknown').replace('os_', '') ) }: ${os_platform.toUpperCase()} [${os_user}]`;
+  activity_data.activity.details = `${ CapitalizeFirstLetter( (icons.os[os_platform] || 'os_unknown').replace('os_', '') ) } ${os_platform} | User: ${os_user}`;
 
-  Update();
-
-  Print('Info', 'Running active window capture');
-
+  // Update
   (async () => {
-    while (1 > 0) { 
-      console.log(activeWindow());
+    while (true) {
+      // Pre-Functions
+      currentWindow = await GetWindow();
+      currentWindow = replaceNames[currentWindow] || currentWindow;
+      currentWindow = currentWindow.replace('.exe', '');
 
-      await setTimeout(5000);
+      // Functions
+      SetWindowData();
+      SetWindowIconData();
+  
+      // Update
+      Update();
+
+      // Timeout
+      await setTimeout(2500);
     }
-  })
+  })();
 })
